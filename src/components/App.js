813d7327584c086b647ca25da7399e8ce7df9a64
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-05-23 23:58:40 -0400

    Done thanks official solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-
 import NavBar from "./NavBar";
-
-// the App component should render out the GifListContainer component
+import GifListContainer from "./GifListContainer";
 
 function App() {
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <GifListContainer />
     </div>
   );
 }
 
 export default App;
+
+/*
+
+https://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g
+
+https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=dc6zaTOxFJmzC&rating=g
+
+*/
---
+++ src/components/GifListContainer.js
@@ -0,0 +1,29 @@
+// copied from official solutions;  I think that I am starting to understand it now;
+import React, { useEffect, useState } from "react";
+import GifList from "./GifList";
+import GifSearch from "./GifSearch";
+
+function GifListContainer() {
+  const [gifs, setGifs] = useState([]);
+  const [query, setQuery] = useState("dolphins");
+
+  useEffect(() => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`
+    )
+      .then((r) => r.json())
+      .then(({ data }) => {
+        const gifs = data.map((gif) => ({ url: gif.images.original.url }));
+        setGifs(gifs);
+      });
+  }, [query]);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <GifList gifs={gifs} />
+      <GifSearch onSubmitQuery={setQuery} />
+    </div>
+  );
+}
+
+export default GifListContainer;
